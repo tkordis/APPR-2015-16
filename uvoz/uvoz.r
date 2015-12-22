e154615ab00f6d986ae146d66dbea466cfebf551
Tim Kordiš <tkordis@gmail.com>
2015-12-22 16:17:58 +0100

    uvoz

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -16,8 +16,7 @@ stran <- GET(data.page, add_headers("User-Agent" = agent,
 text <- content(stran, "text")
 data <- fromJSON(text)
 tabela <- data$playerTableStats
-tabela<-tabela[,c("firstName","lastName","teamName","age","height","weight","positionText","apps","minsPlayed","yellowCard","redCard","rating","subOn","manOfTheMatch","goal","assistTotal","aerialWonPerGame")]
-
+tabela<-tabela[,c("name","teamName","positionText","age","height","weight","apps","minsPlayed","subOn","rating","manOfTheMatch","goal","assistTotal","yellowCard","redCard","aerialWonPerGame")]
 
 #Defensive tabela
 data.page2 <- "http://www.whoscored.com/StatisticsFeed/1/GetPlayerStatistics?category=summary&subcategory=defensive&statsAccumulationType=0&isCurrent=true&playerId=&teamIds=&matchId=&stageId=9155&tournamentOptions=2&sortBy=Rating&sortAscending=&age=&ageComparisonType=&appearances=&appearancesComparisonType=&field=Overall&nationality=&positionOptions=&timeOfTheGameEnd=&timeOfTheGameStart=&isMinApp=true&page=&includeZeroValues=&numberOfPlayersToPick=100"
@@ -59,4 +58,7 @@ tabela4 <- tabela4[,c("totalPassesPerGame","accurateCrossesPerGame","accurateLon
 
 
 skupna.tabela <- cbind(tabela, tabela2, tabela3, tabela4)
-
+imena <- skupna.tabela[,1]
+row.names(skupna.tabela) <- imena
+skupna.tabela$name <- NULL
+colnames(skupna.tabela) <- c("Club","Position","Age","Height","Weight","Apps","Minutes played","Substitute On","Rating","MOTM","Goals","Assists","Yellow Cards","Red Cards","Aerial Won PerGame=PG","Tackle PG","Interception PG","Foul PG","Offside won PG","Clearance PG","Dribbled past PG","Block PG","Own Goals","Shot PG","Key pass PG","Successful dribble PG","Foul given PG","Offside given PG","Disposessed PG","Turnover PG","Passes PG","Accurate crosses PG","Accurate long pass PG","Accurate through ball PG","Pass success")
